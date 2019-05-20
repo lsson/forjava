e6e037657b7ce593b090891acca69f871f6fb4ca
lisson <1902948209@qq.com>
2019-05-20 15:01:29 +0800

    第一次github提交

--- web002/src/web002/Demo.java
+++ web002/src/web002/Demo.java
@@ -3,7 +3,6 @@ package web002;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("泄如今git");
-	System.out.println("是的美貌的父母");
-	System.out.println("是的美貌的父母");
+	System.out.println("是的美貌的父母撒啊啊啊啊啊");
 }
 }
